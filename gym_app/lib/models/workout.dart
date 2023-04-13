b0da704d25e0862ccd364baab06a280a8f56e5c1
TranChiViet <viettranchi7@gmail.com>
2023-04-13 16:36:12 +0700

    add all prj

---
+++ gym_app/lib/models/workout.dart
@@ -0,0 +1,45 @@
+import 'package:equatable/equatable.dart';
+
+import 'exercise.dart';
+
+class Workout extends Equatable {
+  final String? title;
+  final List<Exercise> exercises; 
+
+  const Workout({required this.title, required this.exercises});
+
+  factory Workout.fromJson(Map<String, dynamic> json) {
+    List<Exercise> exercises = [];
+    int index = 0;
+    int startTime = 0;
+    for (var ex in json['exercises'] as Iterable) {
+      exercises.add(Exercise.fromJson(ex, index, startTime));
+      index++;
+      print("..$index...");
+      startTime += exercises.last.prelude! + exercises.last.duration!;
+    }
+    return Workout(title: json['title'] as String?, exercises: exercises);
+  }
+
+  Map<String, dynamic> toJson() => {'title': title, 'exercises': exercises};
+
+  int getTotal() {
+    int time = exercises.fold(
+        0, (previousValue, ex) => previousValue + ex.duration! + ex.prelude!);
+    return time;
+  }
+
+  Exercise gerCurrentExercise(int? elapsed) {
+    return exercises.lastWhere((element) => element.startTime! <= elapsed!);
+  }
+
+  copyWith({String? title}) =>
+      Workout(title: title ?? this.title, exercises: exercises);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [title, exercises];
+
+  @override
+  bool get stringify => true;
+}
---
+++ gym_app/lib/screens/edit_workout_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+import '../blocs/workout_cubit.dart';
+import '../blocs/workouts_cubit.dart';
+import '../helpers.dart';
+import '../models/exercise.dart';
+import '../models/workout.dart';
+import '../states/workout_states.dart';
+import 'edit_excercise_screen.dart';
+
+class EditWorkoutScreen extends StatelessWidget {
+  const EditWorkoutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: BlocBuilder<WorkoutCubit, WorkoutState>(
+          builder: (context, state) {
+            WorkoutEditingState we = state as WorkoutEditingState;
+            return Scaffold(
+              appBar: AppBar(
+                leading: BackButton(
+                  onPressed: () =>
+                      BlocProvider.of<WorkoutCubit>(context).goHome(),
+                ),
+                title: InkWell(
+                  child: Text(we.workout!.title!),
+                  onTap: () => showDialog(
+                      context: context,
+                      builder: (_) {
+                        final controller =
+                            TextEditingController(text: we.workout!.title!);
+                        return AlertDialog(
+                          content: TextField(
+                            controller: controller,
+                            decoration: const InputDecoration(
+                                labelText: "Workout Title"),
+                          ),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  if (controller.text.isNotEmpty) {
+                                    Navigator.pop(context);
+                                    Workout renamedWorkout = we.workout!
+                                        .copyWith(title: controller.text);
+                                    BlocProvider.of<WorkoutsCubit>(context)
+                                        .saveWorkouts(
+                                            renamedWorkout, we.workoutIndex);
+                                    BlocProvider.of<WorkoutCubit>(context)
+                                        .editWorkout(
+                                            renamedWorkout, we.workoutIndex);
+                                  }
+                                },
+                                child: const Text("Rename"))
+                          ],
+                        );
+                      }),
+                ),
+              ),
+              body: ListView.builder(
+                  itemCount: we.workout!.exercises.length,
+                  itemBuilder: (context, index) {
+                    Exercise exercise = we.workout!.exercises[index];
+                    if (we.exIndex == index) {
+                      return EditExerciseScreen(
+                          workout: we.workout,
+                          workoutIndex: we.workoutIndex,
+                          exIndex: we.exIndex);
+                    } else {
+                      return ListTile(
+                        leading: Text(formatTime(exercise.prelude!, true)),
+                        title: Text(exercise.title!),
+                        trailing: Text(formatTime(exercise.duration!, true)),
+                        onTap: () => BlocProvider.of<WorkoutCubit>(context)
+                            .editExercise(index),
+                      );
+                    }
+                  }),
+            );
+          },
+        ),
+        onWillPop: () => BlocProvider.of<WorkoutCubit>(context).goHome());
+  }
+}
