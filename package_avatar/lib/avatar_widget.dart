5450f7e5769c7a74839a584ee1d08f29fb641596
Viet <viettranchi7@gmail.com>
2023-04-17 16:57:01 +0700

    reup

---
+++ package_avatar/lib/avatar_widget.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AvatarWidget extends StatelessWidget {
+  const AvatarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
