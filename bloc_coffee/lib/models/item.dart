0aff27668323fb0a175d40755be9e37f99114138
Viet <viettranchi7@gmail.com>
2023-04-18 17:05:11 +0700

    Reup

--- bloc_coffee/lib/models/item.dart
+++ bloc_coffee/lib/models/item.dart
@@ -18,7 +18,7 @@ List<Item> listItem = [
   Item(id: '1', image: AssetPath.cocoa, name: 'Cinnamon & Cocoa', price: 99),
   Item(id: '2', image: AssetPath.drizzled, name: 'Drizzeled with Caramel', price: 199),
   Item(id: '3', image: AssetPath.burst, name: 'Bursting Blueberry', price: 249),
-  Item(id: '4', image: AssetPath.dalgona, name: 'Dalgona Whipped Macha', price: 299),
+  Item(id: '4', image: AssetPath.burst, name: 'Bursting Blueberry', price: 249),
   Item(id: '5', image: AssetPath.cocoa, name: 'Milk Coffee', price: 49),
   Item(id: '6', image: AssetPath.drizzled, name: 'Milk Foam', price: 199),
 ];
